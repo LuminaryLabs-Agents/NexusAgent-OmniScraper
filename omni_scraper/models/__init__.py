416e3514435931e0ebc3c5b86fbab4710e7dc1d6
thecrimsondeveloper <110572544+thecrimsondeveloper@users.noreply.github.com>
2026-07-06 21:33:14 -0400

    Export LM Studio client

--- omni_scraper/models/__init__.py
+++ omni_scraper/models/__init__.py
@@ -1,1 +1,5 @@
-"""Model integration package."""
+"""Local model clients."""
+
+from .lm_studio import LMStudioClient, LMStudioConfig, ModelCallError
+
+__all__ = ["LMStudioClient", "LMStudioConfig", "ModelCallError"]
