22f8f6054615da78aa492daf8fefcde80b38edfd
thecrimsondeveloper <110572544+thecrimsondeveloper@users.noreply.github.com>
2026-07-06 21:36:56 -0400

    Export reducer utilities

--- omni_scraper/reduce/__init__.py
+++ omni_scraper/reduce/__init__.py
@@ -1,1 +1,7 @@
 """Deterministic page reduction utilities."""
+
+from .fetch import FetchResult, fetch_html
+from .html_to_markdown import ReducedPage, reduce_html
+from .signals import EMAIL_RE, PHONE_RE, URL_RE, collect_signals
+
+__all__ = ["EMAIL_RE", "PHONE_RE", "URL_RE", "FetchResult", "ReducedPage", "collect_signals", "fetch_html", "reduce_html"]
